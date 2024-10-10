f77eb163c50ef1b332ebbedfc9ed601a31c25578
ikitamalarose <laroseikitama@gmail.com>
2024-10-10 10:45:42 +0000

    feat(main.js) update getFragmentId function

--- main.js
+++ main.js
@@ -10,7 +10,7 @@ import {
     createContentQuestion
 } from "./js/utils.js";
 
-
+/* 
 function getFragmentId() {
     const currentUrl = window.location.href;
 
@@ -20,8 +20,24 @@ function getFragmentId() {
     );
 
     return filename;
-}
-
+} */
+    function getFragmentId() {
+        const currentUrl = window.location.href;
+        
+        // Vérifier si l'URL contient un ".html", sinon on utilise le dernier segment du chemin
+        if (currentUrl.includes(".html")) {
+            const filename = currentUrl.substring(
+                currentUrl.lastIndexOf("/") + 1,
+                currentUrl.lastIndexOf(".html")
+            );
+            return filename;
+        } else {
+            // Utiliser le dernier segment du chemin si l'URL ne contient pas de .html
+            const pathSegments = window.location.pathname.split("/");
+            return pathSegments[pathSegments.length - 1] || "index"; // "index" est utilisé si le chemin est vide
+        }
+    }
+    
 
 async function loadDataPerPages() {
 
@@ -103,8 +119,5 @@ async function loadAndCreateMenuItems() {
 
 
 /* loadAndCreateMenuItems(); */
-
-document.addEventListener('DOMContentLoaded', () => {
-    loadDataPerPages();
-});
+loadDataPerPages();
 

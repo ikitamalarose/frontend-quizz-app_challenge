ab2a6612c8d78838b9029de96c416faf985d292f
ikitamalarose <laroseikitama@gmail.com>
2024-10-10 10:52:50 +0000

    feat(main.js) update getFragmentId function

--- main.js
+++ main.js
@@ -21,22 +21,26 @@ function getFragmentId() {
 
     return filename;
 } */
+    
     function getFragmentId() {
         const currentUrl = window.location.href;
-        
-        // Vérifier si l'URL contient un ".html", sinon on utilise le dernier segment du chemin
+        let filename;
+    
+        // Vérifier si l'URL contient un ".html", sinon utiliser le dernier segment du chemin
         if (currentUrl.includes(".html")) {
-            const filename = currentUrl.substring(
+            filename = currentUrl.substring(
                 currentUrl.lastIndexOf("/") + 1,
                 currentUrl.lastIndexOf(".html")
             );
-            return filename;
         } else {
             // Utiliser le dernier segment du chemin si l'URL ne contient pas de .html
             const pathSegments = window.location.pathname.split("/");
-            return pathSegments[pathSegments.length - 1] || "index"; // "index" est utilisé si le chemin est vide
+            filename = pathSegments[pathSegments.length - 1] || "index"; // "index" est utilisé si le chemin est vide
         }
+    
+        return filename;
     }
+    
     
 
 async function loadDataPerPages() {

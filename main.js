46a8871ac6aa97224ba2f197052b0fe6254ec655
ikitamalarose <laroseikitama@gmail.com>
2024-10-10 11:36:36 +0000

    feat(main.js): go back to initial function getFragmentId deploy on Netlify

--- main.js
+++ main.js
@@ -10,7 +10,7 @@ import {
     createContentQuestion
 } from "./js/utils.js";
 
-/* 
+
 function getFragmentId() {
     const currentUrl = window.location.href;
 
@@ -20,28 +20,7 @@ function getFragmentId() {
     );
 
     return filename;
-} */
-    
-    function getFragmentId() {
-        const currentUrl = window.location.href;
-        let filename;
-    
-        // Vérifier si l'URL contient un ".html", sinon utiliser le dernier segment du chemin
-        if (currentUrl.includes(".html")) {
-            filename = currentUrl.substring(
-                currentUrl.lastIndexOf("/") + 1,
-                currentUrl.lastIndexOf(".html")
-            );
-        } else {
-            // Utiliser le dernier segment du chemin si l'URL ne contient pas de .html
-            const pathSegments = window.location.pathname.split("/");
-            filename = pathSegments[pathSegments.length - 1] || "index"; // "index" est utilisé si le chemin est vide
-        }
-    
-        return filename;
-    }
-    
-    
+}    
 
 async function loadDataPerPages() {
 
@@ -118,10 +97,5 @@ async function loadAndCreateMenuItems() {
     }
 }
 
-
-
-
-
-/* loadAndCreateMenuItems(); */
 loadDataPerPages();
 
